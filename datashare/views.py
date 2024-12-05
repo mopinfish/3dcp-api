6d6ad84991f53e0b9b34ce0a8dbe72f71c7d8207
OtsukaNoboru <mopinfish@gmail.com>
2024-12-05 19:03:35 +0900

    add index.html

--- datashare/views.py
+++ datashare/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    params = {
+        'title': '地理空間情報のサイト',
+        'msg': 'これはトップページです。',
+    }
+    return render(request, 'datashare/index.html', params)
