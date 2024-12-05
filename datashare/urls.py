3a8d2c617c3d68a0fff24f1a42b03594e1015650
OtsukaNoboru <mopinfish@gmail.com>
2024-12-05 19:08:28 +0900

    add mypage and links

--- datashare/urls.py
+++ datashare/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'datashare'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('mypage/', views.mypage_funcView, name='mypage'),
 ]
--- datashare/views.py
+++ datashare/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     params = {
-        'title': '地理空間情報のサイト',
+        'title': '地理空間情報の共有サイト',
         'msg': 'これはトップページです。',
+        'goto_mypage': 'datashare:mypage',
     }
     return render(request, 'datashare/index.html', params)
+
+def mypage_funcView(request):
+    params = {
+        'title': '地理空間情報の共有サイト',
+        'msg': 'これはマイページです。',
+        'goto_index': 'datashare:index',
+    }
+    return render(request, 'datashare/mypage.html', params)
