dccdcecf00a79ef545dd1088dcd40e12e6cc9c4c
OtsukaNoboru <mopinfish@gmail.com>
2024-12-21 19:28:08 +0900

    add pgsql settings

--- Makefile
+++ Makefile
@@ -6,3 +6,17 @@ run:
 	python manage.py runserver
 migrate:
 	python manage.py migrate
+makemigrations:
+	python manage.py makemigrations
+
+# temporary commands
+
+## connect database
+pg:
+	docker compose exec postgis psql -U geobase
+
+## list databases
+pg-ls-db:
+	docker compose exec postgis psql -U geobase -c "\l"
+
+
--- my_django/settings.py
+++ my_django/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+# プロジェクトのルートディレクトリを設定
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'my_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geobase',
+        'USER ': 'geobase',
+        'PASSWORD': 'geobase',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+print('---------------------')
+print(DATABASES)
+print('---------------------')
 
 
 # Password validation
