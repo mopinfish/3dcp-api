d64f7fbcf208f37299a0329a7ab60060b864c601
OtsukaNoboru <mopinfish@gmail.com>
2024-12-26 20:24:14 +0900

    add DATABASE setting

--- my_django/settings.py
+++ my_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # プロジェクトのルートディレクトリを設定
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,6 +83,11 @@ WSGI_APPLICATION = 'my_django.wsgi.application'
 
 
 DATABASES = {}
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('POSTGRES_URL')
+    )
+}
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.postgresql',
