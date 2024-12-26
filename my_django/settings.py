1f118d89ad7b746c0271ec0ac876b0037c3bded7
OtsukaNoboru <mopinfish@gmail.com>
2024-12-26 19:15:10 +0900

    database setting is changed to blank temporary.

--- my_django/settings.py
+++ my_django/settings.py
@@ -80,16 +80,18 @@ WSGI_APPLICATION = 'my_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geobase',
-        'USER ': 'geobase',
-        'PASSWORD': 'geobase',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+
+DATABASES = {}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'geobase',
+#        'USER ': 'geobase',
+#        'PASSWORD': 'geobase',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 print('---------------------')
 print(DATABASES)
