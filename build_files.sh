7a5989515cbb265c446c12c3c9d1994c4b6109ef
OtsukaNoboru <mopinfish@gmail.com>
2024-12-31 23:18:37 +0900

    survey2

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 sudo apt-get update
 sudo apt-get install libgdal-dev
 
-which gdal-config
-gdalc-config --version
 pip3 install GDAL
+gdalc-config --version
+which gdal-config
 #export PATH=$PATH:/path/to/gdal-config/directory
 #export GDAL_CONFIG=/path/to/gdal-config
 #export GDAL_VERSION=3.2.0  # インストールされているバージョンに合わせて変更してください
