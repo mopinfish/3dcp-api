1bfcd5f9a9e1c1211d3ee1142378d8f35151762d
OtsukaNoboru <mopinfish@gmail.com>
2025-03-03 12:16:18 +0900

    add taito cultural properties data

--- open3d_map/loaders.py
+++ open3d_map/loaders.py
@@ -2,6 +2,8 @@ import os
 from django.contrib.gis.utils import LayerMapping, LayerMapError
 from .models import CulturalProperty
 
+from django.db import transaction
+
 class CulturalPropertiesLoader:
     mapping = {
         'name': 'name',
@@ -21,9 +23,15 @@ class CulturalPropertiesLoader:
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'cultural_properties.shp'))
 
     @classmethod
+    @transaction.atomic
     def run(cls, verbose=True):
         lm = LayerMapping(CulturalProperty, cls.path, cls.mapping, transform=False, encoding='utf-8')
         try:
-            lm.save(strict=False, verbose=True)
+            with transaction.atomic():
+                lm.save(strict=False, verbose=True)
         except LayerMapError as e:
+            transaction.set_rollback(True)  # トランザクションをロールバック
             print(f"エラーが発生しました: {e}")
+
+class TaitoCulturalPropertiesLoader(CulturalPropertiesLoader):
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'cultural_properties_taito.shp'))
