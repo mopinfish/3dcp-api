27d3e4975ee59e446525ee919ffe50e520f66fae
OtsukaNoboru <mopinfish@gmail.com>
2025-03-06 13:47:30 +0900

    add tag filter

--- open3d_map/serializers.py
+++ open3d_map/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Movie, CulturalProperty, ImageUpload
+from .models import Movie, CulturalProperty, ImageUpload, Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,6 +19,7 @@ class ImageUploadSerializer(serializers.ModelSerializer):
 class CulturalPropertySerializer(serializers.ModelSerializer):
     movies = MovieSerializer(many=True, read_only=True)
     images = ImageUploadSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)  # タグ情報を含める
 
     class Meta:
         model = CulturalProperty
--- open3d_map/views.py
+++ open3d_map/views.py
@@ -1,43 +1,8 @@
 from rest_framework import viewsets
-from django_filters import rest_framework as filters 
-from django.contrib.gis.db.models.functions import Distance
-from django.contrib.gis.geos import Point
-from django_filters.rest_framework import NumberFilter
 
 from .models import Movie, CulturalProperty
 from .serializers import MovieSerializer, CulturalPropertySerializer
-
-class CulturalPropertyFilter(filters.FilterSet):
-    name = filters.CharFilter(lookup_expr='icontains')
-    name_en = filters.CharFilter(lookup_expr='icontains')
-    has_movies = filters.BooleanFilter(field_name='movies', lookup_expr='isnull', exclude=True)
-    
-    # 緯度・経度・距離のフィルタを追加
-    lat = NumberFilter(method='filter_by_distance')
-    lon = NumberFilter(method='filter_by_distance')
-    distance = NumberFilter(method='filter_by_distance')
-
-    class Meta:
-        model = CulturalProperty
-        fields = ['name', 'name_en', 'has_movies', 'lat', 'lon', 'distance']
-
-    def filter_by_distance(self, queryset, name, value):
-        """
-        指定した緯度(lat)、経度(lon)、半径(distance)内のデータのみを取得
-        """
-        lat = self.data.get('lat')
-        lon = self.data.get('lon')
-        distance = self.data.get('distance')
-
-        if lat and lon and distance:
-            try:
-                user_location = Point(float(lon), float(lat), srid=4326)  # ユーザーの座標
-                return queryset.annotate(
-                    distance=Distance('geom', user_location)
-                ).filter(distance__lte=float(distance))  # 指定距離内のデータを取得
-            except ValueError:
-                return queryset  # 不正な値が渡された場合は、フィルタ適用せず返す
-        return queryset
+from .filters import CulturalPropertyFilter
 
 class CulturalPropertyViewSet(viewsets.ModelViewSet):
     queryset = CulturalProperty.objects.all().prefetch_related('movies').prefetch_related('images')
